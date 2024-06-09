877b453da4b1ded3bc313b6e07f784217e93f19d
Tuana <57767763+tuananohut@users.noreply.github.com>
2024-06-09 18:46:10 +0300

    Delete src/build.bat

--- src/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
-set path=c:\Users\Tuana\Masaüstü\Projeler\C++ PROJELER\CustomEngine\CustomEngine\src;%path%;
-mkdir C:\Users\tuana\Masaüstü\PROJELER\C++ PROJELER\CustomEngine\CustomEngine\src\build
-pushd C:\Users\tuana\Masaüstü\PROJELER\C++ PROJELER\CustomEngine\CustomEngine\src\build
-cl "C:\Users\tuana\Masaüstü\PROJELER\C++ PROJELER\CustomEngine\CustomEngine\src\main.cpp" user32.lib Gdi32.lib dinput8.lib dxguid.lib
-popd
