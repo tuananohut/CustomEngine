903bdca9063578d6957ab90f8bbdfc5e94d49239
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-08-28 22:37:44 +0300

    rendertexture.cpp updated

--- src/rendertexture.cpp
+++ src/rendertexture.cpp
@@ -19,5 +19,118 @@ bool RenderTexture::Initialize(ID3D11Device* device,
 							   float screenNear,
 							   int format)
 {
+	D3D11_TEXTURE2D_DESC textureDesc;
+	HRESULT result;
+	D3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc;
+	D3D11_SHADER_RESOURCE_VIEW_DESC shaderResourceViewDesc;
+	D3D11_TEXTURE2D_DESC depthBufferDesc; 
+	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilViewDesc;
+	DXGI_FORMAT textureFormat;
+
+	switch (format)
+	{
+	case 1:
+	{
+		textureFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+		break;
+	}
+	default:
+	{
+		textureFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+		break;
+	}
+	}
+
+	m_textureWidth = textureWidth;
+	m_textureHeight = textureHeight;
+
+	ZeroMemory(&textureDesc, sizeof(textureDesc));
+
+	textureDesc.Width = textureWidth;
+	textureDesc.Height = textureHeight;
+	textureDesc.MipLevels = 1;
+	textureDesc.ArraySize = 1;
+	textureDesc.Format = textureFormat;
+	textureDesc.SampleDesc.Count = 1;
+	textureDesc.Usage = D3D11_USAGE_DEFAULT;
+	textureDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+	textureDesc.CPUAccessFlags = 0;
+	textureDesc.MiscFlags = 0;
+
+	result = device->CreateTexture2D(&textureDesc, NULL, &m_renderTargetTexture);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	renderTargetViewDesc.Format = textureDesc.Format;
+	renderTargetViewDesc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+	renderTargetViewDesc.Texture2D.MipSlice = 0;
+
+	result = device->CreateRenderTargetView(m_renderTargetTexture, &renderTargetViewDesc, &m_renderTargetView);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	shaderResourceViewDesc.Format = textureDesc.Format;
+	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
+	shaderResourceViewDesc.Texture2D.MipLevels = 1;
+
+	result = device->CreateShaderResourceView(m_renderTargetTexture, &shaderResourceViewDesc, &m_shaderResourceView);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	ZeroMemory(&depthBufferDesc, sizeof(depthBufferDesc));
+
+	depthBufferDesc.Width = textureWidth;
+	depthBufferDesc.Height = textureHeight;
+	depthBufferDesc.MipLevels = 1;
+	depthBufferDesc.ArraySize = 1;
+	depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthBufferDesc.SampleDesc.Count = 1;
+	depthBufferDesc.SampleDesc.Quality = 0;
+	depthBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthBufferDesc.CPUAccessFlags = 0;
+	depthBufferDesc.MiscFlags = 0;
+
+	result = device->CreateTexture2D(&depthBufferDesc, NULL, &m_depthStencilBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	ZeroMemory(&depthStencilViewDesc, sizeof(depthStencilViewDesc));
+
+	depthStencilViewDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilViewDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	depthStencilViewDesc.Texture2D.MipSlice = 0;
+
+	result = device->CreateDepthStencilView(m_depthStencilBuffer, &depthStencilViewDesc, &m_depthStencilView);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	m_viewport.Width = (float)textureWidth;
+	m_viewport.Height = (float)textureHeight;
+	m_viewport.MinDepth = 0.f;
+	m_viewport.MaxDepth = 1.f;
+	m_viewport.TopLeftX = 0;
+	m_viewport.TopLeftY = 0;
+
+	m_projectionMatrix = XMMatrixPerspectiveFovLH((3.141592654f / 4.f), ((float)textureWidth) / (float)textureHeight, screenNear, screenDepth);
+
+	m_orthoMatrix = XMMatrixOrthographicLH((float)textureWidth, (float)textureHeight, screenNear, screenDepth);
+
+	return true;
+}
+
+void RenderTexture::Shutdown()
+{
 	
 }
