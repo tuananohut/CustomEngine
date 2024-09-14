79284d481b794f968a32f9c86a988bfaea1095b8
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-09-14 21:57:31 +0300

    camera.h updated

--- src/headers/camera.h
+++ src/headers/camera.h
@@ -22,10 +22,14 @@ public:
 	void Render();
 	void GetViewMatrix(XMMATRIX&);
 
+	void RenderReflection(float);
+	void GetReflectionViewMatrix(XMMATRIX&);
+
 private:
 	float m_positionX, m_positionY, m_positionZ;
 	float m_rotationX, m_rotationY, m_rotationZ;
 	XMMATRIX m_viewMatrix;
+	XMMATRIX m_reflectionViewMatrix;
 };
 
 #endif
