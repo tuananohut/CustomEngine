7f0a90fbd8dc0e8b9872fb476ff258e6198e8426
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-09-22 14:00:33 +0300

    glassshader.h added

---
+++ src/headers/glassshader.h
@@ -0,0 +1,54 @@
+#ifndef _GLASSSHADER_H_
+#define _GLASSSHADER_H_
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <fstream>
+
+using namespace DirectX;
+using namespace std;
+
+class GlassShader
+{
+private:
+	struct MatrixBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+	};
+
+	struct GlassBufferType
+	{
+		float refractionScale;
+		XMFLOAT3 padding;
+	};
+
+public:
+	GlassShader();
+	GlassShader(const GlassShader&);
+	~GlassShader();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*, float);
+
+private:
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*, float);
+	void RenderShader(ID3D11DeviceContext*, int);
+
+private:
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11Buffer* m_matrixBuffer;
+	ID3D11SamplerState* m_sampleState;
+	ID3D11Buffer* m_glassBuffer;
+};
+
+#endif
