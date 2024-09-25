78de8bb8ab7eddbdb3db6c7a6ded054b4677063f
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-09-25 17:48:35 +0300

    sound3d.h added

---
+++ src/headers/sound3d.h
@@ -0,0 +1,53 @@
+#ifndef _SOUND3D_H_
+#define _SOUND3D_H_
+
+#include "directsound.h"
+
+class Sound3D
+{
+private:
+	struct RiffWaveHeaderType
+	{
+		char chunkId[4];
+		unsigned long chunkSize;
+		char format[4];
+	};
+
+	struct SubChunkHeaderType
+	{
+		char subChunkId[4];
+		unsigned long subChunkSize;
+	};
+
+	struct FmtType
+	{
+		unsigned short audioFormat;
+		unsigned short numChannels;
+		unsigned long sampleRate;
+		unsigned long bytesPerSecond;
+		unsigned short blockAlign;
+		unsigned short bitsPerSample;
+	};
+
+public:
+	Sound3D();
+	Sound3D(const Sound3D&);
+	~Sound3D();
+
+	bool LoadTrack(IDirectSound8*, char*, long);
+	void ReleaseTrack();
+
+	bool PlayTrack();
+	bool StopTrack();
+	bool Update3DPosition(float, float, float);
+
+private:
+	bool LoadMonoWaveFile(IDirectSound8*, char*, long);
+	void ReleaseWaveFile();
+
+private:
+	IDirectSound8* m_secondaryBuffer;
+	IDirectSound3DBuffer8* m_secondary3DBuffer;
+};
+
+#endif
