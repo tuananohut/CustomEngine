97f9c8e45530bbec83ac70d029bf359a3f4d8d24
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-09-26 16:17:32 +0300

    xaudiosound.h added

---
+++ src/headers/xaudiosound.h
@@ -0,0 +1,52 @@
+#ifndef _XAUDIOSOUND_H_
+#define _XAUDIOSOUND_H_
+
+#include "xaudio.h"
+
+class XAudioSound
+{
+	struct RiffWaveHeaderType
+	{
+		char chunkId[4];
+		unsigned long chunkSize;
+		char format[4];
+	};
+
+	struct SubChunkHeaderType
+	{
+		char subChunkId[4];
+		unsigned long subChunkSize;
+	};
+
+	struct FmtType
+	{
+		unsigned short auidoFormat;
+		unsigned short numChannels;
+		unsigned long sampleRate;
+		unsigned long bytesPerSecond;
+		unsigned long blockAlign;
+		unsigned short bitsPerSample;
+	};
+
+public:
+	XAudioSound();
+	XAudioSound(const XAudioSound&);
+	~XAudioSound();
+
+	bool LoadTrack(IXAudio2*, char*, float);
+	void ReleaseTrack();
+
+	bool PlayTrack();
+	bool StopTrack();
+
+private:
+	bool LoadStereoWaveFile(IXAudio2*, char, float);
+	void ReleaseWaveFile();
+
+private:
+	unsigned char* m_waveData;
+	XAUDIO2_BUFFER m_audioBuffer;
+	IXAudio2SourceVoice* m_sourceVoice;
+};
+
+#endif
