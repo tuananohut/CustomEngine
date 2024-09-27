fee3d7aca73d5936d29f21fa9c7672752da0dc33
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-09-27 16:10:40 +0300

    xaudio.h updated
    x3daudio.h added

--- src/headers/xaudio.h
+++ src/headers/xaudio.h
@@ -4,6 +4,7 @@
 #pragma comment(lib, "xaudio2.lib")
 
 #include <xaudio2.h>
+#include <x3daudio.h>
 #include <stdio.h>
 
 class XAudio
@@ -15,13 +16,18 @@ public:
 	
 	bool Initialize();
 	void Shutdown();
+	bool Frame(X3DAUDIO_EMITTER, IXAudio2SourceVoice*);
 
 	IXAudio2* GetXAudio2();
 
 private:
 	IXAudio2* m_xAudio2;
 	IXAudio2MasteringVoice* m_masterVoice;
+	X3DAUDIO_HANDLE m_X3DInstance;
+	X3DAUDIO_LISTENER m_listener;
+	XAUDIO2_VOICE_DETAILS m_deviceDetails;
+	float* m_matrixCoefficients; 
+	X3DAUDIO_DSP_SETTINGS m_DSPSettings;
 };
-
 
 #endif
