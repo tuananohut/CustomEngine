8984cad06a75c0a0bf6ce93528458f16686baa4c
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-09-27 16:24:54 +0300

    xaudio.cpp updated
    Frame() function added

--- src/xaudio.cpp
+++ src/xaudio.cpp
@@ -4,6 +4,7 @@ XAudio::XAudio()
 {
 	m_xAudio2 = nullptr;
 	m_masterVoice = nullptr;
+	m_matrixCoefficients = nullptr;
 }
 
 XAudio::XAudio(const XAudio& other) {}
@@ -13,6 +14,7 @@ XAudio::~XAudio() {}
 bool XAudio::Initialize()
 {
 	HRESULT result;
+	DWORD dwChannelMask;
 
 	result = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 	if (FAILED(result))
@@ -32,11 +34,49 @@ bool XAudio::Initialize()
 		return false;
 	}
 
+	m_masterVoice->GetChannelMask(&dwChannelMask);
+
+	result = X3DAudioInitialize(dwChannelMask, X3DAUDIO_SPEED_OF_SOUND, m_X3DInstance);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	ZeroMemory(&m_listener, sizeof(m_listener));
+
+	m_listener.Position.x = 0.f;
+	m_listener.Position.y = 0.f;
+	m_listener.Position.z = 0.f;
+
+	m_listener.OrientFront.x = 0.f;
+	m_listener.OrientFront.y = 0.f;
+	m_listener.OrientFront.z = 1.f;
+
+	m_listener.OrientTop.x = 0.f;
+	m_listener.OrientTop.y = 1.f;
+	m_listener.OrientTop.z = 0.f;
+
+	m_masterVoice->GetVoiceDetails(&m_deviceDetails);
+
+	m_matrixCoefficients = new float[m_deviceDetails.InputChannels];
+
+	ZeroMemory(&m_DSPSettings, sizeof(&m_DSPSettings));
+
+	m_DSPSettings.SrcChannelCount = 1;
+	m_DSPSettings.DstChannelCount = m_deviceDetails.InputChannels;
+	m_DSPSettings.pMatrixCoefficients = m_matrixCoefficients;
+
 	return true;
 }
 
 void XAudio::Shutdown()
 {
+	if (m_matrixCoefficients)
+	{
+		delete[] m_matrixCoefficients;
+		m_matrixCoefficients = nullptr;
+	}
+
 	if (m_masterVoice)
 	{
 		m_masterVoice->DestroyVoice();
@@ -50,6 +90,34 @@ void XAudio::Shutdown()
 	}
 
 	CoUninitialize();
+}
+
+bool XAudio::Frame(X3DAUDIO_EMITTER emitter, IXAudio2SourceVoice* sourceVoice)
+{
+	HRESULT result;
+
+	X3DAudioCalculate(m_X3DInstance,
+					  &m_listener,
+					  &emitter,
+					  X3DAUDIO_CALCULATE_MATRIX |
+					  X3DAUDIO_CALCULATE_DOPPLER |
+					  X3DAUDIO_CALCULATE_LPF_DIRECT |
+					  X3DAUDIO_CALCULATE_REVERB,
+					  &m_DSPSettings);
+
+	result = sourceVoice->SetOutputMatrix(m_masterVoice, 1, m_deviceDetails.InputChannels, m_DSPSettings.pMatrixCoefficients);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	result = sourceVoice->SetFrequencyRatio(m_DSPSettings.DopplerFactor);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	return true;
 }
 
 IXAudio2* XAudio::GetXAudio2()
