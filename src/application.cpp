c3f749bfc6d7f56750ae255641b08ab3eccc2e29
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-10-06 16:44:13 +0300

    application.cpp updated

--- src/application.cpp
+++ src/application.cpp
@@ -5,10 +5,12 @@ Application::Application()
 	m_Direct3D = nullptr;
 	m_Camera = nullptr;
 	m_Model = nullptr;
-	m_DepthShader = nullptr;
-
-	m_XAudio = nullptr;
-	m_TestSound2 = nullptr;
+	m_TextureShader = nullptr;
+	m_Model = nullptr;
+	m_RenderTexture = nullptr;
+	m_FullScreenWindow = nullptr;
+	m_Blur = nullptr;
+	m_BlurShader = nullptr;
 }
 
 
@@ -24,8 +26,7 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 	char textureFilename[128];
 	char textureFilename1[128];
 	char textureFilename2[128];
-	char soundFilename[128];
-
+	int downSampleWidth, downSampleHeight;
 	bool result;
 
 	m_Direct3D = new D3D;
@@ -38,12 +39,12 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
 	m_Camera = new Camera;
 
-	m_Camera->SetPosition(0.0f, 2.0f, -10.0f);
+	m_Camera->SetPosition(0.0f, 0.0f, -10.0f);
 	m_Camera->Render();
 
-	strcpy_s(modelFilename, "../CustomEngine/assets/models/floor.txt");
+	strcpy_s(modelFilename, "../CustomEngine/assets/models/cube.txt");
 
-	strcpy_s(textureFilename, "../CustomEngine/assets/textures/grid01.tga");
+	strcpy_s(textureFilename, "../CustomEngine/assets/textures/stone01.tga");
 	strcpy_s(textureFilename1, "../CustomEngine/assets/textures/noise01.tga");
 	strcpy_s(textureFilename2, "../CustomEngine/assets/textures/alpha01.tga");
 
@@ -51,42 +52,52 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 	result = m_Model->Initialize(m_Direct3D->GetDevice(), m_Direct3D->GetDeviceContext(), modelFilename, textureFilename, textureFilename1, textureFilename2);
 	if (!result)
 	{
-		MessageBox(hwnd, L"Could not initialize the floor model object.", L"Error", MB_OK);
+		MessageBox(hwnd, L"Could not initialize the model object.", L"Error", MB_OK);
 		return false;
 	}
 
-	m_DepthShader = new DepthShader;
-
-	result = m_DepthShader->Initialize(m_Direct3D->GetDevice(), hwnd);
+	m_TextureShader = new TextureShader;
+	result = m_TextureShader->Initialize(m_Direct3D->GetDevice(), hwnd);
 	if (!result)
 	{
-		MessageBox(hwnd, L"Could not initialize the depth shader object.", L"Error", MB_OK | MB_ICONERROR);
+		MessageBox(hwnd, L"Could not initialize the texture shader object.", L"Error", MB_OK);
 		return false;
 	}
 
-
-	m_XAudio = new XAudio;
-	result = m_XAudio->Initialize();
+	m_RenderTexture = new RenderTexture;
+	result = m_RenderTexture->Initialize(m_Direct3D->GetDevice(), screenWidth, screenHeight, SCREEN_NEAR, SCREEN_NEAR, 0);
 	if (!result)
 	{
-		MessageBox(hwnd, L"Could not initialize XAudio.", L"Error", MB_ICONERROR | MB_OK);
+		MessageBox(hwnd, L"Could not initialize the render texture object.", L"Error", MB_OK);
 		return false;
 	}
 
-	m_TestSound2 = new XAudioSound3D;
-
-	strcpy_s(soundFilename, "../CustomEngine/assets/sounds/sound04.wav");
-
-	result = m_TestSound2->LoadTrack(m_XAudio->GetXAudio2(), soundFilename, 1.f);
+	m_FullScreenWindow = new OrthoWindow;
+	result = m_FullScreenWindow->Initialize(m_Direct3D->GetDevice(), screenWidth, screenHeight);
 	if (!result)
 	{
-		MessageBox(hwnd, L"Could not load the test 2 sound.", L"Error", MB_ICONERROR | MB_OK);
+		MessageBox(hwnd, L"Could not initialize the full screen ortho window object.", L"Error", MB_OK);
 		return false;
 	}
 
-	m_TestSound2->Update3DPosition(0.0f, 0.0f, 0.0f);
+	downSampleWidth = screenWidth / 2;
+	downSampleHeight = screenHeight / 2;
 
-	// result = m_TestSound2->PlayTrack();
+	m_Blur = new Blur;
+	result = m_Blur->Initialize(m_Direct3D, downSampleWidth, downSampleHeight, SCREEN_NEAR, SCREEN_DEPTH, screenWidth, screenHeight);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize the blur object.", L"Error", MB_OK);
+		return false;
+	}
+
+	m_BlurShader = new BlurShader;
+	result = m_BlurShader->Initialize(m_Direct3D->GetDevice(), hwnd);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize the blur shader object.", L"Error", MB_OK);
+		return false;
+	}
 
 	return true;
 }
@@ -94,21 +105,39 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
 void Application::Shutdown()
 {
-	if (m_TestSound2)
+	if (m_BlurShader)
 	{
-		m_TestSound2->StopTrack();
-
-		m_TestSound2->ReleaseTrack();
-		delete m_TestSound2;
-		m_TestSound2 = nullptr;
+		m_BlurShader->Shutdown();
+		delete m_BlurShader;
+		m_BlurShader = nullptr;
 	}
 
-
-	if (m_XAudio)
+	if (m_Blur)
 	{
-		m_XAudio->Shutdown();
-		delete m_XAudio;
-		m_XAudio = nullptr;
+		m_Blur->Shutdown();
+		delete m_Blur;
+		m_Blur = nullptr;
+	}
+
+	if (m_FullScreenWindow)
+	{
+		m_FullScreenWindow->Shutdown();
+		delete m_FullScreenWindow;
+		m_FullScreenWindow = nullptr;
+	}
+
+	if (m_RenderTexture)
+	{
+		m_RenderTexture->Shutdown();
+		delete m_RenderTexture;
+		m_RenderTexture = nullptr;
+	}
+
+	if (m_TextureShader)
+	{
+		m_TextureShader->Shutdown();
+		delete m_TextureShader;
+		m_TextureShader = nullptr;
 	}
 
 	if (m_Model)
@@ -116,13 +145,6 @@ void Application::Shutdown()
 		m_Model->Shutdown();
 		delete m_Model;
 		m_Model = nullptr;
-	}
-
-	if (m_DepthShader)
-	{
-		m_DepthShader->Shutdown();
-		delete m_DepthShader;
-		m_DepthShader = nullptr;
 	}
 
 	if (m_Camera)
@@ -156,17 +178,17 @@ bool Application::Frame(Input* Input)
 		rotation += 360.0f;
 	}
 
-	result = SoundProcessing();
+	result = RenderSceneToTexture(rotation);
 	if (!result)
 	{
 		return false;
 	}
 
-	// result = RenderSceneToTexture(rotation);
-	// if (!result)
-	// {
-	// 	return false;
-	// }
+	result = m_Blur->BlurTexture(m_Direct3D, m_Camera, m_RenderTexture, m_TextureShader, m_BlurShader);
+	if (!result)
+	{
+		return false;
+	}
 
 	result = Render(rotation);
 	if (!result)
@@ -177,7 +199,7 @@ bool Application::Frame(Input* Input)
 	return true;
 }
 
-
+/*
 bool Application::SoundProcessing()
 {
 	bool result;
@@ -190,9 +212,9 @@ bool Application::SoundProcessing()
 
 	return true;
 }
+*/
 
 
-/*
 bool Application::RenderSceneToTexture(float rotation)
 {
 	XMMATRIX worldMatrix, viewMatrix, projectionMatrix;
@@ -204,7 +226,7 @@ bool Application::RenderSceneToTexture(float rotation)
 
 	m_Direct3D->GetWorldMatrix(worldMatrix);
 	m_Camera->GetViewMatrix(viewMatrix);
-	m_Direct3D->GetProjectionMatrix(projectionMatrix);
+	m_RenderTexture->GetProjectionMatrix(projectionMatrix);
 
 	worldMatrix = XMMatrixRotationY(rotation);
 
@@ -220,24 +242,23 @@ bool Application::RenderSceneToTexture(float rotation)
 
 	return true;
 }
-*/
+
 
 bool Application::Render(float rotation)
 {
-	XMMATRIX worldMatrix, viewMatrix, projectionMatrix, translateMatrix;
-	XMFLOAT3 cameraPosition, modelPosition;
-	double angle;
-	float pi, rotationY;
+	XMMATRIX worldMatrix, viewMatrix, orthoMatrix;
 	bool result;
 
 	m_Direct3D->BeginScene(0.f, 0.f, 0.f, 1.f);
 
 	m_Direct3D->GetWorldMatrix(worldMatrix);
 	m_Camera->GetViewMatrix(viewMatrix);
-	m_Direct3D->GetProjectionMatrix(projectionMatrix);
+	m_Direct3D->GetOrthoMatrix(orthoMatrix);
+	
+	m_FullScreenWindow->Render(m_Direct3D->GetDeviceContext());
 
 	m_Model->Render(m_Direct3D->GetDeviceContext());
-	result = m_DepthShader->Render(m_Direct3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix);
+	result = m_TextureShader->Render(m_Direct3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, orthoMatrix, m_RenderTexture->GetShaderResourceView());
 	if (!result)
 	{
 		return false;
