90d5b101072afc4f24b986f6c30905601d00c535
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-10-07 19:57:55 +0300

    camera.cpp updated

--- src/camera.cpp
+++ src/camera.cpp
@@ -20,8 +20,6 @@ void Camera::SetPosition(float x, float y, float z)
 	m_positionX = x;
 	m_positionY = y;
 	m_positionZ = z; 
-
-	return;
 }
 
 void Camera::SetRotation(float x, float y, float z)
@@ -29,8 +27,6 @@ void Camera::SetRotation(float x, float y, float z)
 	m_rotationX = x;
 	m_rotationY = y;
 	m_rotationZ = z;
-
-	return;
 }
 
 XMFLOAT3 Camera::GetPosition()
@@ -80,14 +76,11 @@ void Camera::Render()
 	lookAtVector = XMVectorAdd(positionVector, lookAtVector);
 
 	m_viewMatrix = XMMatrixLookAtLH(positionVector, lookAtVector, upVector);
-
-	return;
 }
 
 void Camera::GetViewMatrix(XMMATRIX& viewMatrix)
 {
 	viewMatrix = m_viewMatrix;
-	return;
 }
 
 void Camera::RenderReflection(float height)
