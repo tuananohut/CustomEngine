a15c66cb971e07403f70e3354a868f5bb679b0d8
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-10-25 21:47:56 +0300

    particlesystem.cpp updated
    - not completed

--- src/particlesystem.cpp
+++ src/particlesystem.cpp
@@ -108,5 +108,139 @@ void ParticleSystem::ReleaseTexture()
 
 bool ParticleSystem::InitializeParticleSystem() 
 {
+	int i;
 
+	m_particleDeviationX = 0.5f;
+	m_particleDeviationY = 0.1f;
+	m_particleDeviationZ = 2.f;
+
+	m_particleVelocity = 1.f;
+	m_particleVelocityVariation = 0.2f;
+
+	m_particleSize = 0.2f;
+
+	m_particlesPerSecond = 100.f;
+
+	m_maxParticles = 1000;
+
+	m_particleList = new ParticleType[m_maxParticles];
+
+	for (i = 0; i < m_maxParticles; i++)
+	{
+		m_particleList[i].active = false;
+	}
+
+	m_currentParticleCount = 0;
+
+	m_accumulatedTime = 0.f;
+
+	return true;
+}
+
+void ParticleSystem::ShutdownParticleSystem()
+{
+	if (m_particleList)
+	{
+		delete[] m_particleList;
+		m_particleList = nullptr;
+	}
+}
+
+bool ParticleSystem::InitializeBuffers(ID3D11Device* device)
+{
+	unsigned long* indices;
+	int i;
+	D3D11_BUFFER_DESC vertexBufferDesc, indexBufferDesc;
+	D3D11_SUBRESOURCE_DATA vertexData, indexData;
+	HRESULT result;
+
+	m_vertexCount = m_maxParticles * 6;
+
+	m_indexCount = m_vertexCount;
+
+	m_vertices = new VertexType[m_vertexCount];
+
+	indices = new unsigned long[m_indexCount];
+
+	memset(m_vertices, 0, (sizeof(VertexType) * m_vertexCount));
+
+	for (i = 0; i < m_indexCount; i++)
+	{
+		indices[i] = i;
+	}
+
+	vertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	vertexBufferDesc.ByteWidth = sizeof(VertexType) * m_vertexCount;
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	vertexBufferDesc.MiscFlags = 0;
+	vertexBufferDesc.StructureByteStride = 0;
+
+	vertexData.pSysMem = m_vertices;
+	vertexData.SysMemPitch = 0;
+	vertexData.SysMemSlicePitch = 0;
+
+	result = device->CreateBuffer(&vertexBufferDesc, &vertexData, &m_vertexBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	indexBufferDesc.ByteWidth = sizeof(unsigned long) * m_indexCount;
+	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	indexBufferDesc.CPUAccessFlags = 0;
+	indexBufferDesc.MiscFlags = 0;
+	indexBufferDesc.StructureByteStride = 0;
+
+	indexData.pSysMem = indices;
+	indexData.SysMemPitch = 0;
+	indexData.SysMemSlicePitch = 0;
+
+	result = device->CreateBuffer(&indexBufferDesc, &indexData, &m_indexBuffer);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	delete[] indices;
+	indices = nullptr;
+
+	return true;
+}
+
+void ParticleSystem::ShutdownBuffers()
+{
+	if (m_indexBuffer)
+	{
+		m_indexBuffer->Release();
+		m_indexBuffer = nullptr;
+	}
+
+	if (m_vertexBuffer)
+	{
+		m_vertexBuffer->Release();
+		m_vertexBuffer = nullptr;
+	}
+
+	if (m_vertices)
+	{	
+		delete[] m_vertices;
+		m_vertices = nullptr;
+	}
+}
+
+void ParticleSystem::RenderBuffers(ID3D11DeviceContext* deviceContext)
+{
+	unsigned int stride;
+	unsigned int offset;
+
+	stride = sizeof(VertexType);
+	offset = 0;
+
+	deviceContext->IASetVertexBuffers(0, 1, &m_vertexBuffer, &stride, &offset);
+
+	deviceContext->IASetIndexBuffer(m_indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 }
