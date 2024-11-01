6f922723ac1ff9eb11f390b1fd57f8bc78000b9a
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-11-01 18:57:49 +0300

    projectionshader.h updated

--- src/headers/projectionshader.h
+++ src/headers/projectionshader.h
@@ -21,6 +21,20 @@ private:
 		XMMATRIX projection2;
 	};
 
+	struct LightPositionBufferType
+	{
+		XMFLOAT3 lightPosition;
+		float padding;
+	};
+
+	struct LightBufferType
+	{
+		XMFLOAT4 ambientColor;
+		XMFLOAT4 diffuseColor;
+		float brightness;
+		XMFLOAT3 padding;
+	};
+
 public:
 	ProjectionShader();
 	ProjectionShader(const ProjectionShader&);
@@ -28,14 +42,14 @@ public:
 
 	bool Initialize(ID3D11Device*, HWND);
 	void Shutdown();
-	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*);
+	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*, XMFLOAT4, XMFLOAT4, XMFLOAT3, float);
 
 private:
 	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
 	void ShutdownShader();
 	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
 
-	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*);
+	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*, XMFLOAT4, XMFLOAT4, XMFLOAT3, float);
 	void RenderShader(ID3D11DeviceContext*, int);
 
 private:
@@ -44,6 +58,8 @@ private:
 	ID3D11InputLayout* m_layout;
 	ID3D11Buffer* m_matrixBuffer;
 	ID3D11SamplerState* m_sampleState;
+	ID3D11Buffer* m_lightPositionBuffer;
+	ID3D11Buffer* m_lightBuffer;
 };
 
 #endif
