b2e91a4fa3a8a2bde69a01661e4aee1177c996a7
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-11-06 21:35:10 +0300

    shadowshader.h uploaded

---
+++ src/headers/shadowshader.h
@@ -0,0 +1,58 @@
+#ifndef _SHADOWSHADER_H_
+#define _SHADOWSHADER_H_
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <fstream>
+
+using namespace DirectX;
+using namespace std;
+
+class ShadowShader
+{
+private:
+	struct MatrixBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+		XMMATRIX lightView;
+		XMMATRIX lightProjection;
+	};
+
+	struct LightPositionBufferType
+	{
+		XMFLOAT3 lightPosition;
+		float padding;
+	};
+
+	struct LightBufferType
+	{
+		XMFLOAT4 ambientColor;
+		XMFLOAT4 diffuseColor;
+		float bias;
+		XMFLOAT3 padding;
+	};
+
+public: 
+	ShadowShader();
+	ShadowShader(const ShadowShader&);
+	~ShadowShader();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, ID3D11ShaderResourceView*, XMFLOAT4, XMFLOAT4, XMFLOAT3, float);
+
+private: 
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11Buffer* m_matrixBuffer;
+	ID3D11SamplerState* m_sampleStateClamp;
+	ID3D11SamplerState* m_sampleStateWrap;
+	ID3D11Buffer* m_lightPositionBuffer;
+	ID3D11Buffer* m_lightBuffer;
+};
+
+#endif
