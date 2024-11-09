3d03eddba7fb21b243e05f2d7e648d26bfa66756
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-11-09 21:12:38 +0300

    application.h updated

--- src/headers/application.h
+++ src/headers/application.h
@@ -5,14 +5,17 @@
 #include "input.h"
 #include "camera.h"
 #include "model.h"
-#include "projectionshader.h"
-#include "viewpoint.h"
 #include "light.h"
+#include "rendertexture.h"
+#include "depthshader.h"
+#include "shadowshader.h"
 
 const bool FULL_SCREEN = true;
 const bool VSYNC_ENABLED = true;
-const float SCREEN_DEPTH = 1000.0f;
-const float SCREEN_NEAR = 0.3f;
+const float SCREEN_DEPTH = 100.0f;
+const float SCREEN_NEAR = 1.f;
+const int SHADOWMAP_WIDTH = 1024;
+const int SHADOWMAP_HEIGHT = 1024;
 
 class Application
 {
@@ -28,17 +31,20 @@ public:
 private:
 	// bool RenderSceneToTexture(float, float, float, bool);
 	// bool SoundProcessing();
+	bool RenderDepthToTexture();
 	bool Render(float);
 
 private:
 	D3D* m_Direct3D;
 	Camera* m_Camera;
-	Model* m_GroundModel;
 	Model* m_CubeModel;
-	ProjectionShader* m_ProjectionShader;
-	Texture* m_ProjectionTexture;
-	ViewPoint* m_ViewPoint;
+	Model* m_SphereModel;
+	Model* m_GroundModel;
 	Light* m_Light;
+	RenderTexture* m_RendereTexture;
+	DepthShader* m_DepthShader;
+	ShadowShader* m_ShadowShader;
+	float m_shadowMapBias;
 };
 
 #endif
