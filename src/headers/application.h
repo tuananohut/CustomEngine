3e33255c4ffc9bd0165be56c470556fc1601b35f
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-11-10 14:22:19 +0300

    application.h updated
    - assignment 4 completed

--- src/headers/application.h
+++ src/headers/application.h
@@ -14,8 +14,8 @@ const bool FULL_SCREEN = true;
 const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 100.0f;
 const float SCREEN_NEAR = 1.f;
-const int SHADOWMAP_WIDTH = 1024;
-const int SHADOWMAP_HEIGHT = 1024;
+const int SHADOWMAP_WIDTH = 256;
+const int SHADOWMAP_HEIGHT = 256;
 
 class Application
 {
