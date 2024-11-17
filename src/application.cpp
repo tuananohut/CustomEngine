cc9fe3b725421344e8a22c5f876c1bbf5ba506e7
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-11-17 19:24:22 +0300

    application.cpp updated

--- src/application.cpp
+++ src/application.cpp
@@ -9,8 +9,10 @@ Application::Application()
     m_GroundModel = nullptr;
     m_Light = nullptr;
     m_Light2 = nullptr;
+    m_Light3 = nullptr;
     m_RenderTexture = nullptr;
     m_RenderTexture2 = nullptr;
+    m_RenderTexture3 = nullptr;
     m_DepthShader = nullptr;
     m_ShadowShader = nullptr;
 }
@@ -84,7 +86,7 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
     m_Light->SetAmbientColor(0.15f, 0.15f, 0.15f, 1.0f);
     m_Light->SetDiffuseColor(1.0f, 1.0f, 1.0f, 1.0f);
-    m_Light->SetLookAt(0.0f, 10.0f, 5.0f);
+    m_Light->SetLookAt(0.0f, 0.0f, 0.0f);
     m_Light->GenerateProjectionMatrix(SCREEN_DEPTH, SCREEN_NEAR);
 
     m_RenderTexture = new RenderTexture;
@@ -116,11 +118,25 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
     m_Light2 = new Light;
 
     m_Light2->SetDiffuseColor(0.2f, 0.1f, 0.8f, 1.0f);
-    m_Light2->SetLookAt(0.0f, 0.0f, 5.0f);
+    m_Light2->SetLookAt(0.0f, 8.0f, 5.0f);
     m_Light2->GenerateProjectionMatrix(SCREEN_DEPTH, SCREEN_NEAR);
 
     m_RenderTexture2 = new RenderTexture;
     result = m_RenderTexture2->Initialize(m_Direct3D->GetDevice(), SHADOWMAP_WIDTH, SHADOWMAP_HEIGHT, SCREEN_DEPTH, SCREEN_NEAR, 1);
+    if (!result)
+    {
+        MessageBox(hwnd, L"Could not initialize the second render to texture object.", L"Error", MB_OK);
+        return false;
+    }
+
+    m_Light3 = new Light;
+
+    m_Light3->SetDiffuseColor(0.98f, 0.537f, 0.906f, 1.0f);
+    m_Light3->SetLookAt(2.0f, 10.0f, 10.0f);
+    m_Light3->GenerateProjectionMatrix(SCREEN_DEPTH, SCREEN_NEAR);
+
+    m_RenderTexture3 = new RenderTexture;
+    result = m_RenderTexture3->Initialize(m_Direct3D->GetDevice(), SHADOWMAP_WIDTH, SHADOWMAP_HEIGHT, SCREEN_DEPTH, SCREEN_NEAR, 1);
     if (!result)
     {
         MessageBox(hwnd, L"Could not initialize the second render to texture object.", L"Error", MB_OK);
@@ -143,6 +159,19 @@ void Application::Shutdown()
     {
         delete m_Light2;
         m_Light2 = nullptr;
+    }
+
+    if (m_RenderTexture3)
+    {
+        m_RenderTexture3->Shutdown();
+        delete m_RenderTexture3;
+        m_RenderTexture3 = nullptr;
+    }
+
+    if (m_Light3)
+    {
+        delete m_Light3;
+        m_Light3 = nullptr;
     }
 
     if (m_ShadowShader)
@@ -211,6 +240,7 @@ bool Application::Frame(Input* Input)
 {
     static float rotation = 0;
     bool result;
+    XMMATRIX translateMatrix = {}, lightViewMatrix = {}, lightProjectionMatrix = {};
 
     if (Input->IsEscapePressed())
     {
@@ -223,13 +253,19 @@ bool Application::Frame(Input* Input)
     m_Light2->SetPosition(-5.0f, 8.0f, -5.0f);
     m_Light2->GenerateViewMatrix();
 
-    result = RenderDepthToTexture();
+    result = RenderDepthToTexture(translateMatrix, lightViewMatrix, lightProjectionMatrix, m_RenderTexture, m_Light);
     if (!result)
     {
         return false;
     }
 
-    result = RenderDepthToTexture2();
+    result = RenderDepthToTexture(translateMatrix, lightViewMatrix, lightProjectionMatrix, m_RenderTexture2, m_Light2);
+    if (!result)
+    {
+        return false;
+    }
+
+    result = RenderDepthToTexture(translateMatrix, lightViewMatrix, lightProjectionMatrix, m_RenderTexture3, m_Light3);
     if (!result)
     {
         return false;
@@ -244,9 +280,8 @@ bool Application::Frame(Input* Input)
     return true;
 }
 
-bool Application::RenderDepthToTexture()
+bool Application::RenderDepthToTexture(XMMATRIX translateMatrix, XMMATRIX lightViewMatrix, XMMATRIX lightProjectionMatrix, RenderTexture* m_RenderTexture, Light* m_Light)
 {
-    XMMATRIX translateMatrix, lightViewMatrix, lightProjectionMatrix;
     bool result;
 
     m_RenderTexture->SetRenderTarget(m_Direct3D->GetDeviceContext());
@@ -289,53 +324,9 @@ bool Application::RenderDepthToTexture()
     return true;
 }
 
-bool Application::RenderDepthToTexture2()
-{
-    XMMATRIX translateMatrix, lightViewMatrix, lightProjectionMatrix;
-    bool result;
-
-    m_RenderTexture2->SetRenderTarget(m_Direct3D->GetDeviceContext());
-    m_RenderTexture2->ClearRenderTarget(m_Direct3D->GetDeviceContext(), 0.0f, 0.0f, 0.0f, 1.0f);
-
-    m_Light2->GetViewMatrix(lightViewMatrix);
-    m_Light2->GetProjectionMatrix(lightProjectionMatrix);
-
-    translateMatrix = XMMatrixTranslation(-2.0f, 2.0f, 0.0f);
-
-    m_CubeModel->Render(m_Direct3D->GetDeviceContext());
-    result = m_DepthShader->Render(m_Direct3D->GetDeviceContext(), m_CubeModel->GetIndexCount(), translateMatrix, lightViewMatrix, lightProjectionMatrix);
-    if (!result)
-    {
-        return false;
-    }
-
-    translateMatrix = XMMatrixTranslation(2.0f, 2.0f, 0.0f);
-
-    m_SphereModel->Render(m_Direct3D->GetDeviceContext());
-    result = m_DepthShader->Render(m_Direct3D->GetDeviceContext(), m_SphereModel->GetIndexCount(), translateMatrix, lightViewMatrix, lightProjectionMatrix);
-    if (!result)
-    {
-        return false;
-    }
-
-    translateMatrix = XMMatrixTranslation(0.0f, 1.0f, 0.0f);
-
-    m_GroundModel->Render(m_Direct3D->GetDeviceContext());
-    result = m_DepthShader->Render(m_Direct3D->GetDeviceContext(), m_GroundModel->GetIndexCount(), translateMatrix, lightViewMatrix, lightProjectionMatrix);
-    if (!result)
-    {
-        return false;
-    }
-
-    m_Direct3D->SetBackBufferRenderTarget();
-    m_Direct3D->ResetViewport();
-
-    return true;
-}
-
 bool Application::Render(float rotation)
 {
-    XMMATRIX worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, lightViewMatrix2, lightProjectionMatrix2;
+    XMMATRIX worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, lightViewMatrix2, lightProjectionMatrix2, lightViewMatrix3, lightProjectionMatrix3;
     bool result;
 
     m_Direct3D->BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
@@ -350,11 +341,14 @@ bool Application::Render(float rotation)
     m_Light2->GetViewMatrix(lightViewMatrix2);
     m_Light2->GetProjectionMatrix(lightProjectionMatrix2);
 
+    m_Light3->GetViewMatrix(lightViewMatrix3);
+    m_Light3->GetProjectionMatrix(lightProjectionMatrix3);
+
     worldMatrix = XMMatrixTranslation(-2.0f, 2.0f, 0.0f);
 
     m_CubeModel->Render(m_Direct3D->GetDeviceContext());
 
-    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_CubeModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_CubeModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias, lightViewMatrix2, lightProjectionMatrix2, m_RenderTexture2->GetShaderResourceView(), m_Light2->GetPosition(), m_Light2->GetDiffuseColor());
+    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_CubeModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_CubeModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias, lightViewMatrix2, lightProjectionMatrix2, m_RenderTexture2->GetShaderResourceView(), m_Light2->GetPosition(), m_Light2->GetDiffuseColor(), lightViewMatrix3, lightProjectionMatrix3, m_RenderTexture3->GetShaderResourceView(), m_Light3->GetPosition(), m_Light3->GetDiffuseColor());
     if (!result)
     {
         return false;
@@ -363,7 +357,7 @@ bool Application::Render(float rotation)
     worldMatrix = XMMatrixTranslation(2.0f, 2.0f, 0.0f);
 
     m_SphereModel->Render(m_Direct3D->GetDeviceContext());
-    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_SphereModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_SphereModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias, lightViewMatrix2, lightProjectionMatrix2, m_RenderTexture2->GetShaderResourceView(), m_Light2->GetPosition(), m_Light2->GetDiffuseColor());
+    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_SphereModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_SphereModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias, lightViewMatrix2, lightProjectionMatrix2, m_RenderTexture2->GetShaderResourceView(), m_Light2->GetPosition(), m_Light2->GetDiffuseColor(), lightViewMatrix3, lightProjectionMatrix3, m_RenderTexture3->GetShaderResourceView(), m_Light3->GetPosition(), m_Light3->GetDiffuseColor());
     if (!result)
     {
         return false;
@@ -372,7 +366,7 @@ bool Application::Render(float rotation)
     worldMatrix = XMMatrixTranslation(0.0f, 1.0f, 0.0f);
 
     m_GroundModel->Render(m_Direct3D->GetDeviceContext());
-    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_GroundModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_GroundModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias, lightViewMatrix2, lightProjectionMatrix2, m_RenderTexture2->GetShaderResourceView(), m_Light2->GetPosition(), m_Light2->GetDiffuseColor());
+    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_GroundModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_GroundModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias, lightViewMatrix2, lightProjectionMatrix2, m_RenderTexture2->GetShaderResourceView(), m_Light2->GetPosition(), m_Light2->GetDiffuseColor(), lightViewMatrix3, lightProjectionMatrix3, m_RenderTexture3->GetShaderResourceView(), m_Light3->GetPosition(), m_Light3->GetDiffuseColor());
     if (!result)
     {
         return false;
