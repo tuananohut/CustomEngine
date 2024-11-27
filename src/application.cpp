76a7574aa0fead3d41f42f584a46f85bc1bca6d1
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-11-27 17:03:01 +0300

    application.cpp updated

--- src/application.cpp
+++ src/application.cpp
@@ -9,8 +9,13 @@ Application::Application()
     m_GroundModel = nullptr;
     m_Light = nullptr;
     m_RenderTexture = nullptr;
+    m_BlackWhiteRenderTexture = nullptr;
     m_DepthShader = nullptr;
     m_ShadowShader = nullptr;
+    m_SoftShadowShader = nullptr;
+    m_Blur = nullptr;
+    m_TextureShader = nullptr;
+    m_BlurShader = nullptr;
 }
 
 Application::Application(const Application& other) {}
@@ -23,6 +28,7 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
     char textureFilename[128];
     char textureFilename1[128];
     char textureFilename2[128];
+    int downSampleWidth, downSampleHeight;
     bool result;
 
     m_Direct3D = new D3D;
@@ -36,8 +42,12 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
     m_Camera = new Camera;
 
-    m_Camera->SetPosition(-8.0f, 7.0f, 8.0f);
-    m_Camera->SetRotation(35.0f, 135.0f, 0.0f);
+    m_Camera->SetPosition(0.f, 0.f, -10.0f);
+    m_Camera->Render();
+    m_Camera->RenderBaseViewMatrix();
+
+    m_Camera->SetPosition(0.f, 7.f, -10.0f);
+    m_Camera->SetRotation(35.0f, 0.f, 0.f);
     m_Camera->Render();
 
     m_CubeModel = new Model;
@@ -82,13 +92,22 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
     m_Light->SetAmbientColor(0.15f, 0.15f, 0.15f, 1.f);
     m_Light->SetDiffuseColor(1.f, 1.f, 1.f, 1.f);
-    m_Light->GenerateOrthoMatrix(20.f, SHADOWMAP_NEAR, SHADOWMAP_DEPTH);
+    m_Light->SetLookAt(0.f, 0.f, 0.f);
+    m_Light->GenerateProjectionMatrix(SCREEN_DEPTH, SCREEN_NEAR);
 
     m_RenderTexture = new RenderTexture;
-    result = m_RenderTexture->Initialize(m_Direct3D->GetDevice(), SHADOWMAP_WIDTH, SHADOWMAP_HEIGHT, SHADOWMAP_DEPTH, SHADOWMAP_NEAR, 1);
+    result = m_RenderTexture->Initialize(m_Direct3D->GetDevice(), SHADOWMAP_WIDTH, SHADOWMAP_HEIGHT, SCREEN_DEPTH, SCREEN_NEAR, 1);
     if (!result)
     {
         MessageBox(hwnd, L"Could not initialize the render texture object.", L"Error", MB_OK);
+        return false;
+    }
+
+    m_BlackWhiteRenderTexture = new RenderTexture;
+    result = m_BlackWhiteRenderTexture->Initialize(m_Direct3D->GetDevice(), SHADOWMAP_WIDTH, SHADOWMAP_HEIGHT, SCREEN_DEPTH, SCREEN_NEAR, 1);
+    if (!result)
+    {
+        MessageBox(hwnd, L"Could not initialize the black an white render texture object.", L"Error", MB_OK | MB_ICONERROR);
         return false;
     }
 
@@ -108,6 +127,41 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
         return false;
     }
 
+    m_SoftShadowShader = new SoftShadowShader;
+    result = m_SoftShadowShader->Initialize(m_Direct3D->GetDevice(), hwnd);
+    if (!result)
+    {
+        MessageBox(hwnd, L"Could not initialize the soft shadow shader object.", L"Error", MB_OK | MB_ICONERROR);
+        return false;
+    }
+
+    downSampleWidth = SHADOWMAP_WIDTH / 2;
+    downSampleHeight = SHADOWMAP_HEIGHT / 2;
+
+    m_Blur = new Blur;
+    result = m_Blur->Initialize(m_Direct3D, downSampleWidth, downSampleHeight, SCREEN_NEAR, SCREEN_DEPTH, SHADOWMAP_WIDTH, SHADOWMAP_HEIGHT);
+    if (!result)
+    {
+        MessageBox(hwnd, L"Could not initialize the blur object.", L"Error", MB_OK | MB_ICONERROR);
+        return false;
+    }
+
+    m_TextureShader = new TextureShader;
+    result = m_TextureShader->Initialize(m_Direct3D->GetDevice(), hwnd);
+    if (!result)
+    {
+        MessageBox(hwnd, L"Could not initialize the texture shader object.", L"Error", MB_OK | MB_ICONERROR);
+        return false;
+    }
+
+    m_BlurShader = new BlurShader;
+    result = m_BlurShader->Initialize(m_Direct3D->GetDevice(), hwnd);
+    if (!result)
+    {
+        MessageBox(hwnd, L"Could not initialize the blur shader object.", L"Error", MB_OK | MB_ICONERROR);
+        return false;
+    }
+
     m_shadowMapBias = 0.0022f;
     
     return true;
@@ -115,6 +169,34 @@ bool Application::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 
 void Application::Shutdown()
 {
+    if (m_BlurShader)
+    {
+        m_BlurShader->Shutdown();
+        delete m_BlurShader;
+        m_BlurShader = nullptr;
+    }
+
+    if (m_TextureShader)
+    {
+        m_TextureShader->Shutdown();
+        delete m_TextureShader;
+        m_TextureShader = nullptr;
+    }
+
+    if (m_Blur)
+    {
+        m_Blur->Shutdown();
+        delete m_Blur;
+        m_Blur = nullptr;
+    }
+
+    if (m_SoftShadowShader)
+    {
+        m_SoftShadowShader->Shutdown();
+        delete m_SoftShadowShader;
+        m_SoftShadowShader = nullptr;
+    }
+
     if (m_ShadowShader)
     {
         m_ShadowShader->Shutdown();
@@ -134,6 +216,13 @@ void Application::Shutdown()
         m_RenderTexture->Shutdown();
         delete m_RenderTexture;
         m_RenderTexture = nullptr;
+    }
+
+    if (m_BlackWhiteRenderTexture)
+    {
+        m_BlackWhiteRenderTexture->Shutdown();
+        delete m_BlackWhiteRenderTexture;
+        m_BlackWhiteRenderTexture = nullptr;
     }
 
     if (m_Light)
@@ -180,10 +269,7 @@ void Application::Shutdown()
 bool Application::Frame(Input* Input)
 {
     static float rotation = 0;
-    static float lightAngle = 270.f;
-    float radians;
-    float frameTime;
-    static float lightPosX = 9.f;
+    static float lightPosX = -5.f;
     bool result;
     XMMATRIX translateMatrix = {}, lightViewMatrix = {}, lightOrthoMatrix = {};
 
@@ -192,30 +278,31 @@ bool Application::Frame(Input* Input)
         return false;
     }
 
-    frameTime = 10.f;
-
-    lightPosX -= 0.003f * frameTime;
-
-    lightAngle -= 0.03f * frameTime;
-    if (lightAngle < 90.f)
+    lightPosX += 0.05f;
+    if (lightPosX > 5.f)
     {
-        lightAngle = 270.f;
+        lightPosX = -5.f;
+    } 
 
-        lightPosX = 9.f;
-    }
-
-    radians = lightAngle * 0.0174532925f;
-
-    m_Light->SetDirection(sinf(radians), cosf(radians), 0.f);
-
-    m_Light->SetPosition(lightPosX, 8.0f, -0.1f);
-    m_Light->SetLookAt(-lightPosX, 0.f, 0.f);
+    m_Light->SetPosition(lightPosX, 8.0f, -5.f);
     m_Light->GenerateViewMatrix();
 
     result = RenderDepthToTexture(translateMatrix, lightViewMatrix, lightOrthoMatrix, m_RenderTexture, m_Light);
     if (!result)
     {
         return false;
+    }
+
+    result = RenderBlackAndWhiteShadows();
+    if (!result)
+    {
+        return false;
+    }
+
+    result = m_Blur->BlurTexture(m_Direct3D, m_Camera, m_BlackWhiteRenderTexture, m_TextureShader, m_BlurShader);
+    if (!result)
+    {
+        return true;
     }
     
     result = Render(rotation);
@@ -271,9 +358,56 @@ bool Application::RenderDepthToTexture(XMMATRIX translateMatrix, XMMATRIX lightV
     return true;
 }
 
+bool Application::RenderBlackAndWhiteShadows()
+{
+    XMMATRIX translateMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix;
+    bool result;
+
+    m_BlackWhiteRenderTexture->SetRenderTarget(m_Direct3D->GetDeviceContext());
+    m_BlackWhiteRenderTexture->ClearRenderTarget(m_Direct3D->GetDeviceContext(), 0.f, 0.f, 0.f, 1.f);
+
+    m_Camera->GetViewMatrix(viewMatrix);
+    m_Direct3D->GetProjectionMatrix(projectionMatrix);
+
+    m_Light->GetViewMatrix(lightViewMatrix);
+    m_Light->GetProjectionMatrix(lightProjectionMatrix);
+
+    translateMatrix = XMMatrixTranslation(-2.f, 2.f, 0.f);
+
+    m_CubeModel->Render(m_Direct3D->GetDeviceContext());
+    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_CubeModel->GetIndexCount(), translateMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_RenderTexture->GetShaderResourceView(), m_Light->GetPosition(), m_shadowMapBias);
+    if (!result)
+    {
+        return false;
+    }
+
+    translateMatrix = XMMatrixTranslation(2.f, 2.f, 0.f);
+    
+    m_SphereModel->Render(m_Direct3D->GetDeviceContext());
+    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_SphereModel->GetIndexCount(), translateMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_RenderTexture->GetShaderResourceView(), m_Light->GetPosition(), m_shadowMapBias);
+    if (!result)
+    {
+        return false;
+    }
+
+    translateMatrix = XMMatrixTranslation(0.f, 1.f, 0.f);
+
+    m_GroundModel->Render(m_Direct3D->GetDeviceContext());
+    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_GroundModel->GetIndexCount(), translateMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix, m_RenderTexture->GetShaderResourceView(), m_Light->GetPosition(), m_shadowMapBias);
+    if (!result)
+    {
+        return false;
+    }
+
+    m_Direct3D->SetBackBufferRenderTarget();
+    m_Direct3D->ResetViewport();
+
+    return true;
+}
+
 bool Application::Render(float rotation)
 {
-    XMMATRIX worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightOrthoMatrix, lightViewMatrix2, lightOrthoMatrix2, lightViewMatrix3, lightOrthoMatrix3;
+    XMMATRIX worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightProjectionMatrix;
     bool result;
 
     m_Direct3D->BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
@@ -283,13 +417,12 @@ bool Application::Render(float rotation)
     m_Direct3D->GetProjectionMatrix(projectionMatrix);
 
     m_Light->GetViewMatrix(lightViewMatrix);
-    m_Light->GetOrthoMatrix(lightOrthoMatrix);
+    m_Light->GetProjectionMatrix(lightProjectionMatrix);
 
     worldMatrix = XMMatrixTranslation(-2.0f, 2.0f, 0.0f);
 
     m_CubeModel->Render(m_Direct3D->GetDeviceContext());
-
-    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_CubeModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightOrthoMatrix, m_CubeModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetDirection(), m_shadowMapBias);
+    result = m_SoftShadowShader->Render(m_Direct3D->GetDeviceContext(), m_CubeModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_CubeModel->GetTexture(0), m_BlackWhiteRenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias);
     if (!result)
     {
         return false;
@@ -298,7 +431,7 @@ bool Application::Render(float rotation)
     worldMatrix = XMMatrixTranslation(2.0f, 2.0f, 0.0f);
 
     m_SphereModel->Render(m_Direct3D->GetDeviceContext());
-    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_SphereModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightOrthoMatrix, m_SphereModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetDirection(), m_shadowMapBias);
+    result = m_SoftShadowShader->Render(m_Direct3D->GetDeviceContext(), m_SphereModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_SphereModel->GetTexture(0), m_BlackWhiteRenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias);
     if (!result)
     {
         return false;
@@ -307,7 +440,7 @@ bool Application::Render(float rotation)
     worldMatrix = XMMatrixTranslation(0.0f, 1.0f, 0.0f);
 
     m_GroundModel->Render(m_Direct3D->GetDeviceContext());
-    result = m_ShadowShader->Render(m_Direct3D->GetDeviceContext(), m_GroundModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, lightViewMatrix, lightOrthoMatrix, m_GroundModel->GetTexture(0), m_RenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetDirection(), m_shadowMapBias);
+    result = m_SoftShadowShader->Render(m_Direct3D->GetDeviceContext(), m_GroundModel->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix, m_GroundModel->GetTexture(0), m_BlackWhiteRenderTexture->GetShaderResourceView(), m_Light->GetAmbientColor(), m_Light->GetDiffuseColor(), m_Light->GetPosition(), m_shadowMapBias);
     if (!result)
     {
         return false;
