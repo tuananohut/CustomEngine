9ca462eeda21014cad299ddd64e2c05ab1c21619
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-12-16 22:30:53 +0300

    deferredbuffers.h uploaded

---
+++ src/headers/deferredbuffers.h
@@ -0,0 +1,36 @@
+#ifndef _DEFERREDBUFFERS_H_
+#define _DEFERREDBUFFERS_H_
+
+const int BUFFER_COUNT = 2;
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+class DeferredBuffers
+{
+public:
+	DeferredBuffers();
+	DeferredBuffers(const DeferredBuffers&);
+	~DeferredBuffers();
+
+	bool Initialize(ID3D11Device*, int, int, float, float);
+	void Shutdown();
+
+	void SetRenderTargets(ID3D11DeviceContext*);
+	void ClearRenderTargets(ID3D11DeviceContext*, float, float, float, float);
+
+	ID3D11ShaderResourceView* GetShaderResourceView(int);
+
+private:
+	int m_textureWidth, m_textureHeight;
+	ID3D11Texture2D* m_renderTargetTextureArray[BUFFER_COUNT];
+	ID3D11RenderTargetView* m_renderTargetViewArray[BUFFER_COUNT];
+	ID3D11ShaderResourceView* m_shaderResourceViewArray[BUFFER_COUNT];
+	ID3D11Texture2D* m_depthStencilBuffer;
+	ID3D11DepthStencilView* m_depthStencilView;
+	D3D11_VIEWPORT m_viewport;
+};
+
+#endif
