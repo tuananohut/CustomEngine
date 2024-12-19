22493b4b7ae240ffd9cbf56541f29a905745d7a3
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-12-19 09:31:39 +0300

    deferreedshader.cpp updated

--- src/deferredshader.cpp
+++ src/deferredshader.cpp
@@ -2,11 +2,11 @@
 
 DeferredShader::DeferredShader()
 {
-	m_vertexShader = nullptr; 
-	m_pixelShader = nullptr; 
-	m_layout = nullptr; 
-	m_sampleStateWrap = nullptr; 
-	m_matrixBuffer = nullptr; 
+	m_vertexShader = nullptr;
+	m_pixelShader = nullptr;
+	m_layout = nullptr;
+	m_sampleStateWrap = nullptr;
+	m_matrixBuffer = nullptr;
 }
 
 DeferredShader::DeferredShader(const DeferredShader& other) {}
@@ -16,7 +16,7 @@ DeferredShader::~DeferredShader() {}
 bool DeferredShader::Initialize(ID3D11Device* device, HWND hwnd)
 {
 	wchar_t vsFilename[128], psFilename[128];
-	int error; 
+	int error;
 	bool result;
 
 	error = wcscpy_s(vsFilename, 128, L"src/shaders/deferred.vs");
@@ -45,9 +45,15 @@ void DeferredShader::Shutdown()
 	ShutdownShader();
 }
 
-bool DeferredShader::Render(ID3D11DeviceContext* deviceContext, int indexCount, XMMATRIX worldMatrix, XMMATRIX viewMatrix, XMMATRIX projectionMatrix, ID3D11ShaderResourceView* texture)
+
+bool DeferredShader::Render(ID3D11DeviceContext* deviceContext, 
+							int indexCount, 
+							XMMATRIX worldMatrix, 
+							XMMATRIX viewMatrix,
+							XMMATRIX projectionMatrix, 
+							ID3D11ShaderResourceView* texture)
 {
-	bool result; 
+	bool result;
 
 	result = SetShaderParameters(deviceContext, worldMatrix, viewMatrix, projectionMatrix, texture);
 	if (!result)
@@ -60,14 +66,15 @@ bool DeferredShader::Render(ID3D11DeviceContext* deviceContext, int indexCount, 
 	return true;
 }
 
+
 bool DeferredShader::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vsFilename, WCHAR* psFilename)
 {
-	HRESULT result; 
+	HRESULT result;
 	ID3D10Blob* errorMessage = nullptr;
 	ID3D10Blob* vertexShaderBuffer = nullptr;
-	ID3D10Blob* pixelShaderBuffer = nullptr; 
+	ID3D10Blob* pixelShaderBuffer = nullptr;
 	D3D11_INPUT_ELEMENT_DESC polygonLayout[3];
-	unsigned int numElements; 
+	unsigned int numElements;
 	D3D11_SAMPLER_DESC samplerDesc;
 	D3D11_BUFFER_DESC matrixBufferDesc;
 
@@ -120,7 +127,7 @@ bool DeferredShader::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vs
 	polygonLayout[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
 	polygonLayout[0].InputSlot = 0;
 	polygonLayout[0].AlignedByteOffset = 0;
-	polygonLayout[0].InputSlot = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	polygonLayout[0].InstanceDataStepRate = 0;
 
 	polygonLayout[1].SemanticName = "TEXCOORD";
@@ -128,7 +135,7 @@ bool DeferredShader::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vs
 	polygonLayout[1].Format = DXGI_FORMAT_R32G32_FLOAT;
 	polygonLayout[1].InputSlot = 0;
 	polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-	polygonLayout[1].InputSlot = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	polygonLayout[1].InstanceDataStepRate = 0;
 
 	polygonLayout[2].SemanticName = "NORMAL";
@@ -136,7 +143,7 @@ bool DeferredShader::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vs
 	polygonLayout[2].Format = DXGI_FORMAT_R32G32B32_FLOAT;
 	polygonLayout[2].InputSlot = 0;
 	polygonLayout[2].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-	polygonLayout[2].InputSlot = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[2].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
 	polygonLayout[2].InstanceDataStepRate = 0;
 
 	numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
@@ -157,7 +164,7 @@ bool DeferredShader::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vs
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-	samplerDesc.MipLODBias = 0.f; 
+	samplerDesc.MipLODBias = 0.0f;
 	samplerDesc.MaxAnisotropy = 1;
 	samplerDesc.ComparisonFunc = D3D11_COMPARISON_ALWAYS;
 	samplerDesc.BorderColor[0] = 0;
@@ -173,10 +180,10 @@ bool DeferredShader::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vs
 		return false;
 	}
 
-	matrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC; 
+	matrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
 	matrixBufferDesc.ByteWidth = sizeof(MatrixBufferType);
 	matrixBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-	matrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE; 
+	matrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
 	matrixBufferDesc.MiscFlags = 0;
 	matrixBufferDesc.StructureByteStride = 0;
 
@@ -185,28 +192,17 @@ bool DeferredShader::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vs
 	{
 		return false;
 	}
-	
+
 	return true;
 }
 
+
 void DeferredShader::ShutdownShader()
 {
-	if (m_vertexShader)
+	if (m_matrixBuffer)
 	{
-		m_vertexShader->Release();
-		m_vertexShader = nullptr;
-	}
-
-	if (m_pixelShader)
-	{
-		m_pixelShader->Release();
-		m_pixelShader = nullptr;
-	}
-
-	if (m_layout)
-	{
-		m_layout->Release();
-		m_layout = nullptr;
+		m_matrixBuffer->Release();
+		m_matrixBuffer = nullptr;
 	}
 
 	if (m_sampleStateWrap)
@@ -215,17 +211,28 @@ void DeferredShader::ShutdownShader()
 		m_sampleStateWrap = nullptr;
 	}
 
-	if (m_matrixBuffer)
+	if (m_layout)
 	{
-		m_matrixBuffer->Release();
-		m_matrixBuffer = nullptr;
+		m_layout->Release();
+		m_layout = nullptr;
+	}
+
+	if (m_pixelShader)
+	{
+		m_pixelShader->Release();
+		m_pixelShader = nullptr;
+	}
+
+	if (m_vertexShader)
+	{
+		m_vertexShader->Release();
+		m_vertexShader = nullptr;
 	}
 }
 
-
 void DeferredShader::OutputShaderErrorMessage(ID3D10Blob* errorMessage, HWND hwnd, WCHAR* shaderFilename)
 {
-	char* compileErrors; 
+	char* compileErrors;
 	unsigned long long bufferSize, i;
 	ofstream fout;
 
@@ -245,15 +252,17 @@ void DeferredShader::OutputShaderErrorMessage(ID3D10Blob* errorMessage, HWND hwn
 	errorMessage->Release();
 	errorMessage = nullptr;
 
-	MessageBox(hwnd, L"Error compiling shader. Check shader-error.txt for message.", shaderFilename, MB_OK | MB_ICONERROR);
+	MessageBox(hwnd, L"Error compiling shader.  Check shader-error.txt for message.", shaderFilename, MB_OK);
 }
 
-bool DeferredShader::SetShaderParameters(ID3D11DeviceContext* deviceContext, XMMATRIX worldMatrix, XMMATRIX viewMatrix, XMMATRIX projectionMatrix, ID3D11ShaderResourceView* texture)
+bool DeferredShader::SetShaderParameters(ID3D11DeviceContext* deviceContext, XMMATRIX worldMatrix, XMMATRIX viewMatrix,
+	XMMATRIX projectionMatrix, ID3D11ShaderResourceView* texture)
 {
 	HRESULT result;
 	D3D11_MAPPED_SUBRESOURCE mappedResource;
 	unsigned int bufferNumber;
 	MatrixBufferType* dataPtr;
+
 
 	worldMatrix = XMMatrixTranspose(worldMatrix);
 	viewMatrix = XMMatrixTranspose(viewMatrix);
@@ -293,5 +302,3 @@ void DeferredShader::RenderShader(ID3D11DeviceContext* deviceContext, int indexC
 
 	deviceContext->DrawIndexed(indexCount, 0, 0);
 }
-
-
