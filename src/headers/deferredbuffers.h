69acc1fb8ad83ae5b06a8a498e7f7947880850d9
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2024-12-27 21:50:25 +0300

    deferredbuffers.h updated

--- src/headers/deferredbuffers.h
+++ src/headers/deferredbuffers.h
@@ -1,7 +1,7 @@
 #ifndef _DEFERREDBUFFERS_H_
 #define _DEFERREDBUFFERS_H_
 
-const int BUFFER_COUNT = 2;
+const int BUFFER_COUNT = 3;
 
 #include <d3d11.h>
 #include <DirectXMath.h>
@@ -21,7 +21,18 @@ public:
 	void SetRenderTargets(ID3D11DeviceContext*);
 	void ClearRenderTargets(ID3D11DeviceContext*, float, float, float, float);
 
-	ID3D11ShaderResourceView* GetShaderResourceView(int);
+	ID3D11ShaderResourceView* GetShaderResourcePositions();
+	ID3D11ShaderResourceView* GetShaderResourceNormals();
+	ID3D11ShaderResourceView* GetShaderResourceColors();
+
+	void GetProjectionMatrix(XMMATRIX&);
+	void GetOrthoMatrix(XMMATRIX&);
+
+	int GetTextureWidth();
+	int GetTextureHeight();
+
+private: 
+	bool BuildRenderTexture(DXGI_FORMAT, int, ID3D11Device*);
 
 private:
 	int m_textureWidth, m_textureHeight;
@@ -31,6 +42,8 @@ private:
 	ID3D11Texture2D* m_depthStencilBuffer;
 	ID3D11DepthStencilView* m_depthStencilView;
 	D3D11_VIEWPORT m_viewport;
+	XMMATRIX m_projectionMatrix;
+	XMMATRIX m_orthoMatrix;
 };
 
 #endif
