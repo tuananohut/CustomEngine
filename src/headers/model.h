6ad700e1237787bbf3afabfde8b7feb0afd37603
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2025-01-05 17:55:24 +0300

    model.h updated

--- src/headers/model.h
+++ src/headers/model.h
@@ -18,8 +18,8 @@ private:
 		XMFLOAT3 position;
 		XMFLOAT2 texture;
 		XMFLOAT3 normal;
-		// XMFLOAT3 tangent;
-		// XMFLOAT3 binormal;
+		XMFLOAT3 tangent;
+		XMFLOAT3 binormal;
 	};
 
 	/*
@@ -35,11 +35,10 @@ private:
 		float x, y, z;
 		float tu, tv;
 		float nx, ny, nz;
-		// float tx, ty, tz;
-		// float bx, by, bz;
+		float tx, ty, tz;
+		float bx, by, bz;
 	};
 
-	/*
 	struct TempVertexType
 	{
 		float x, y, z;
@@ -51,14 +50,14 @@ private:
 	{
 		float x, y, z;
 	};
-	*/
+	
 
 public:
 	Model();
 	Model(const Model&);
 	~Model();
 
-	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, char*, char*, char*);
+	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, char*, char*, char*, char*);
 	void Shutdown();
 	void Render(ID3D11DeviceContext*);
 
@@ -72,14 +71,14 @@ private:
 	void ShutdownBuffers();
 	void RenderBuffers(ID3D11DeviceContext*);
 
-	bool LoadTextures(ID3D11Device*, ID3D11DeviceContext*, char*, char*);
+	bool LoadTextures(ID3D11Device*, ID3D11DeviceContext*, char*, char*, char*);
 	void ReleaseTextures();
 
 	bool LoadModel(char*);
 	void ReleaseModel();
 	
-	// void CalculateModelVectors();
-	// void CalculateTangentBinormal(TempVertexType, TempVertexType, TempVertexType, VectorType&, VectorType&);
+	void CalculateModelVectors();
+	void CalculateTangentBinormal(TempVertexType, TempVertexType, TempVertexType, VectorType&, VectorType&);
 
 private:
 	ID3D11Buffer* m_vertexBuffer;
