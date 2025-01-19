2ccb3d626e301af9bfd4e163139ff9c102ddf1a3
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2025-01-19 21:28:43 +0300

    parallaxscroll.h uploaded & application.h updated

--- src/headers/application.h
+++ src/headers/application.h
@@ -5,14 +5,9 @@
 #include "input.h"
 #include "timer.h"
 #include "camera.h"
-#include "model.h"
-#include "light.h"
 #include "orthowindow.h"
-#include "rendertexture.h"
-#include "blur.h"
-#include "lightshader.h"
-#include "heat.h"
-#include "heatshader.h"
+#include "parallaxscroll.h"
+#include "scrollshader.h"
 
 const bool FULL_SCREEN = true;
 const bool VSYNC_ENABLED = true;
@@ -31,9 +26,9 @@ public:
 	bool Frame(Input*);
 
 private:
-	bool RenderHeatToTexture();
-	bool RenderSceneToTexture(float);
-	bool Render(float);
+	bool Render();
+	// bool RenderHeatToTexture();
+	// bool RenderSceneToTexture(float);
 	// bool RenderGBuffer();
 	// bool RenderSSAO();
 	// bool BlurSSAOTexture();
@@ -45,17 +40,9 @@ private:
 	D3D* m_Direct3D;
 	Timer* m_Timer;
 	Camera* m_Camera;
-	Model* m_Model;
-	Light* m_Light;
-	LightShader* m_LightShader; 
-	RenderTexture* m_RenderTexture; 
 	OrthoWindow* m_FullScreenWindow; 
-	TextureShader* m_TextureShader;
-	BlurShader* m_BlurShader;
-	Blur* m_Blur;
-	Heat* m_Heat; 
-	HeatShader* m_HeatShader; 
-	RenderTexture* m_HeatTexture; 
+	ScrollShader* m_ScrollShader; 
+	ParallaxScroll* m_ParallaxForest; 
 };
 
 #endif
---
+++ src/headers/parallaxscroll.h
@@ -0,0 +1,37 @@
+#ifndef _PARALLAXSCROLL_H_
+#define _PARALLAXSCROLL_H_
+
+#include <fstream>
+using namespace std;
+
+#include "texture.h"
+
+class ParallaxScroll
+{
+private:
+	struct ParallaxArrayType
+	{
+		float scrollSpeed;
+		float textureTranslation;
+	};
+
+public: 
+	ParallaxScroll();
+	ParallaxScroll(const ParallaxScroll&);
+	~ParallaxScroll();
+
+	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, char*);
+	void Shutdown(); 
+	void Frame(float); 
+
+	int GetTextureCount(); 
+	ID3D11ShaderResourceView* GetTexture(int); 
+	float GetTranslation(int); 
+
+private:
+	Texture* m_TextureArray; 
+	ParallaxArrayType* m_ParallaxArray;
+	int m_textureCount; 
+};
+
+#endif
