37847651da48ae7d53acad661420352770fae90a
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2025-01-22 17:42:49 +0300

    particleshader.h updated

--- src/headers/particleshader.h
+++ src/headers/particleshader.h
@@ -41,7 +41,7 @@ private:
 	ID3D11PixelShader* m_pixelShader;
 	ID3D11InputLayout* m_layout;
 	ID3D11Buffer* m_matrixBuffer;
-	ID3D11SamplerState* m_sampleState;
+	ID3D11SamplerState* m_sampleStateWrap;
 };
 
 #endif
