999c2cea70cbcc3676e94dd2bc66358f584a9ae8
Tuana Nohut <57767763+tuananohut@users.noreply.github.com>
2025-01-23 17:17:30 +0300

    d3d.h updated

--- src/headers/d3d.h
+++ src/headers/d3d.h
@@ -14,53 +14,56 @@ using namespace DirectX;
 class D3D
 {
 public:
-  D3D();
-  D3D(const D3D&);
-  ~D3D();
+	D3D();
+	D3D(const D3D&);
+	~D3D();
 
-  bool Initialize(int, int, bool, HWND, bool, float, float);
-  void Shutdown();
+	bool Initialize(int, int, bool, HWND, bool, float, float);
+	void Shutdown();
 
-  void BeginScene(float, float, float, float);
-  void EndScene();
+	void BeginScene(float, float, float, float);
+	void EndScene();
 
-  ID3D11Device* GetDevice();
-  ID3D11DeviceContext* GetDeviceContext();
-  
-  void GetProjectionMatrix(XMMATRIX&);
-  void GetWorldMatrix(XMMATRIX&);
-  void GetOrthoMatrix(XMMATRIX&);
-  
-  void GetVideoCardInfo(char*, int&);
+	ID3D11Device* GetDevice();
+	ID3D11DeviceContext* GetDeviceContext();
 
-  void SetBackBufferRenderTarget();
-  void ResetViewport();
-  
-  void TurnZBufferOn();
-  void TurnZBufferOff();
+	void GetProjectionMatrix(XMMATRIX&);
+	void GetWorldMatrix(XMMATRIX&);
+	void GetOrthoMatrix(XMMATRIX&);
 
-  void EnableAlphaBlending();
-  void DisableAlphaBlending();
-  
+	void GetVideoCardInfo(char*, int&);
+
+	void SetBackBufferRenderTarget();
+	void ResetViewport();
+
+	void TurnZBufferOn();
+	void TurnZBufferOff();
+
+	void EnableAlphaBlending();
+	void DisableAlphaBlending();
+
+	void EnableParticleAlphaBlending();
+
 private:
-  bool m_vsync_enabled;
-  int m_videoCardMemory;
-  char m_videoCardDescription[128];
-  IDXGISwapChain* m_swapChain;
-  ID3D11Device* m_device;
-  ID3D11DeviceContext* m_deviceContext;
-  ID3D11RenderTargetView* m_renderTargetView;
-  ID3D11Texture2D* m_depthStencilBuffer;
-  ID3D11DepthStencilState* m_depthStencilState;
-  ID3D11DepthStencilState* m_depthDisabledStencilState;
-  ID3D11DepthStencilView* m_depthStencilView;
-  ID3D11RasterizerState* m_rasterState;
-  XMMATRIX m_projectionMatrix;
-  XMMATRIX m_worldMatrix;
-  XMMATRIX m_orthoMatrix;
-  D3D11_VIEWPORT m_viewport;
-  ID3D11BlendState* m_alphaEnableBlendingState;
-  ID3D11BlendState* m_alphaDisableBlendingState;
+	bool m_vsync_enabled;
+	int m_videoCardMemory;
+	char m_videoCardDescription[128];
+	IDXGISwapChain* m_swapChain;
+	ID3D11Device* m_device;
+	ID3D11DeviceContext* m_deviceContext;
+	ID3D11RenderTargetView* m_renderTargetView;
+	ID3D11Texture2D* m_depthStencilBuffer;
+	ID3D11DepthStencilState* m_depthStencilState;
+	ID3D11DepthStencilState* m_depthDisabledStencilState;
+	ID3D11DepthStencilView* m_depthStencilView;
+	ID3D11RasterizerState* m_rasterState;
+	XMMATRIX m_projectionMatrix;
+	XMMATRIX m_worldMatrix;
+	XMMATRIX m_orthoMatrix;
+	D3D11_VIEWPORT m_viewport;
+	ID3D11BlendState* m_alphaEnableBlendingState;
+	ID3D11BlendState* m_alphaDisableBlendingState;
+	ID3D11BlendState* m_alphaParticleEnableBlendingState; 
 };
 
 #endif
